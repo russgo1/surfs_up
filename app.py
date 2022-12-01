eb0a8dab7ecf3860be0dfe074205a8efa6f07388
russgo1 <russellg580@gmail.com>
2022-12-01 12:30:04 -0500

    9.5.5

--- app.py
+++ app.py
@@ -24,5 +24,39 @@ session = Session(engine)
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def welcome():
+    return('Welcome to the Climate Analysis API!\n'
+        'Available Routes:\n'
+        '/api/v1.0/precipitation\n'
+        '/api/v1.0/stations\n'
+        '/api/v1.0/tobs\n'
+        '/api/v1.0/temp/start/end'
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    
+    return jsonify(precip)
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+
+    return jsonify(stations=stations)
+
+
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+
+    return jsonify(temps=temps)
